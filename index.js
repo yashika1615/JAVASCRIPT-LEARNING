a876acead2f71b9c7f7e8fffb65a76cb4489d102
Yashika1615 <katariay22@gmail.com>
2024-03-17 19:49:42 +0530

    save

---
+++ index.js
@@ -0,0 +1,27 @@
+//functions
+function sayHello(){
+    console.log("hello")
+}
+
+//calling of functions
+sayHello()
+sayHello()
+sayHello()
+sayHello()
+sayHello()
+
+//to access functions we have parameters
+function add(a,b){
+   console.log(a+b)
+}
+
+add(5,10)
+add(2,8)
+
+//a and b are arguments
+//during function call when we pass the 
+//value we call it as parameters
+//during function definition it is arguments
+//during function call it is parameters
+
+
