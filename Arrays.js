777f11cfade2eb623dfcb2528fd32aea6ccc7ab3
Yashika1615 <katariay22@gmail.com>
2024-03-18 22:32:30 +0530

    higher order function in js

--- Arrays.js
+++ Arrays.js
@@ -14,4 +14,15 @@ console.log(students[1]);
 students[1]="hello";
 console.log(students);
 
+students.push("bye");
+students.push("hello","hi","yeyy");
+
+console.log(students);
+const myArr=[1,"hello","&"];
+myArr.push("true");
+//you can make heterogeneous array in javascript
+
+console.log(myArr.indexOf("true"));
+console.log(myArr.pop());
+myArr.reverse();
 
