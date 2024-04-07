84583e01af70bf2338bdd03c876cc891cc46be65
Yashika1615 <katariay22@gmail.com>
2024-04-07 22:45:08 +0530

    variables

---
+++ classwork and js by piyush garg/file2.js
@@ -0,0 +1,23 @@
+//string
+let x="hello";
+console.log(x);
+console.log(typeof(x));
+//number
+let y=1.4;
+console.log(y);
+console.log(typeof(y));
+
+let z=5;
+console.log(z)
+console.log(typeof(z));
+
+//boolean
+let m=true;
+console.log(m)
+console.log(typeof(z));
+
+//undefined
+let w;
+console.log
+w="9";
+console.log(typeof(w));
