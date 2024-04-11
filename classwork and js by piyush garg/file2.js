67111cb672958cb6df8189fe7032f27b3d92687e
Yashika1615 <katariay22@gmail.com>
2024-04-11 19:29:02 +0530

    javascript by technical suneja

--- classwork and js by piyush garg/file2.js
+++ classwork and js by piyush garg/file2.js
@@ -63,6 +63,62 @@ createCourse('skjndc');
 //1.mempry phase- variable env- memory allocated
 //2.code phase-thread of execution
 
+//window and this
+console.log(a);
+var f=10;
+
+console.log(a);
+console.log(this.a);
+console.log(window);
+console.log(this.a==window);
+
+//let, var and const
+//const and let are more strict use const 
+// whenever you want you want your value to be constant 
+// and it cannot be changed , use let otherwise avoid using var
+
+//lexical scope- you basically check where it is located and 
+// can we access it or not
+
+function hello(){
+    let x=10;
+    console.log(x);
+}
+
+hello();
+
+//function - first class citizens
+//higher order functions- functions that takes one more function as an argument or returns a function as a result
+
+// function addno(a,b){
+//     return a+b;
+// }
+
+// console.log(add)
+// console.log(add(2,3))
+
+let sum=function(a,b){
+    return a+b
+}
+
+let diff=function(a,b){
+    return a-b
+}
+
+function operate(operationfunc,a,b){
+       return operatefunction(a,b)
+}
+
+console.log(sum)
+console.log(sum(2,3))
+
+
+console.log(sum)
+console.log(sum(2,3))
+
+
+
+
 
 
 
