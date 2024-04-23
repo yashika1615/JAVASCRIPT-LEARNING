0e9e958a00d559d783649d7a543e1ac91f936da1
Yashika1615 <katariay22@gmail.com>
2024-04-24 00:47:21 +0530

    tic tac toe game using javascript by apna college

--- classwork and js by piyush garg/b.js
+++ classwork and js by piyush garg/b.js
@@ -1,3 +1,10 @@
-alert("hello");
+// alert("hello");
 
-console.log("ehbfua");
+// console.log("ehbfua");
+// window.console.log("hejdja");
+
+// console.log(window);
+// console.dir(document);
+// console.dir(document.body);
+// console.dir(document.body.childNodes[1]);
+
